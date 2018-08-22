79cc86d77dd58e8a52307f5f74e88beff3e04850
soun059 <42586808+soun059@users.noreply.github.com>
2018-08-22 14:04:10 +0530

    Updating the algorithm
    updating the algorithm for better use of other data sets

--- classification_knn.py
+++ classification_knn.py
@@ -5,7 +5,8 @@ import random
 
 def dataset(filename,drop_name,class_name,split_size):
     df=pd.read_csv(filename);
-    df.drop(drop_name,1,inplace=True);
+    if drop_name!='':
+        df.drop(drop_name,1,inplace=True);
     x1=np.array(df);
     #y1=np.array(df[class_name]);
     train_set=[]
@@ -15,8 +16,8 @@ def dataset(filename,drop_name,class_name,split_size):
                 train_set.append(x1[i])
             else:
                 test_set.append(x1[i])
-    train_set=np.array(train_set,dtype=np.int16)
-    test_set=np.array(test_set,dtype=np.int16)
+    train_set=np.array(train_set,dtype=np.float64)
+    test_set=np.array(test_set,dtype=np.float64)
     return train_set,test_set
 
 def min_dist(distance,avoid):
@@ -55,7 +56,7 @@ def accuracy(krr_test,test):
     tp=0
     fp=0
     for i in range(len(krr_test)):
-        if krr_test[i]==test[i][5]:
+        if krr_test[i]==test[i][len(test[0])-1]:
             tp+=1
         else:
             fp+=1
@@ -71,14 +72,15 @@ def euclidian_dist(train_set,test_set):
                 e_distance+=((features[i]-test_set[x][i])**2)
             distance.append([sqrt(e_distance),features[len(features)-1]])
         krr_test.append(k_nearest_neighbours(distance,5))
-        return accuracy(krr_test,test_set);
+    for i in range(len(krr_test)):
+        print(krr_test[i],test_set[i][len(test_set[0])-1])
+    return accuracy(krr_test,test_set);
 
 def main():
-    filename='cryo.csv'
-    drop_name='sex'
-    class_name='Result_of_Treatment'
+    filename='ex2data1.csv'         
+    drop_name=''
+    class_name='class'
     train,test=dataset(filename,drop_name,class_name,split_size=0.8)
     acc=euclidian_dist(train,test)
     print(acc)
 main()
-    
